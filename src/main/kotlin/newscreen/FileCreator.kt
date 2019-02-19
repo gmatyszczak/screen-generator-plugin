ae93307e0a92b0f452d6d573857970e3faa2d4f8
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-02-19 15:48:23 +0100

    Save files to source directory.

--- src/main/kotlin/newscreen/FileCreator.kt
+++ src/main/kotlin/newscreen/FileCreator.kt
@@ -3,6 +3,7 @@ package newscreen
 import com.intellij.lang.java.JavaLanguage
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
 import settings.ScreenGeneratorComponent
@@ -16,8 +17,13 @@ class FileCreatorImpl(private val project: Project) : FileCreator {
 
     override fun createScreenFiles(screenName: String) {
         ApplicationManager.getApplication().runWriteAction {
+            val sourceRoots = ProjectRootManager.getInstance(project).contentSourceRoots.filter {
+                !it.path.contains("build", true)
+                        && !it.path.contains("test", true)
+                        && !it.path.contains("res")
+            }
             val screenElements = ScreenGeneratorComponent.getInstance(project).settings.screenElements
-            val directory = PsiManager.getInstance(project).findDirectory(project.baseDir)
+            val directory = PsiManager.getInstance(project).findDirectory(sourceRoots[0])
             screenElements.forEach {
                 val file = PsiFileFactory.getInstance(project).createFileFromText("$screenName${it.name}.kt", JavaLanguage.INSTANCE, "Text")
                 directory!!.add(file)
