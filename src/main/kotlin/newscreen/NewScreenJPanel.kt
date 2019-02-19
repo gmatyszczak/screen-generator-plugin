9bd3a2a60d83374e9f56f5f9e3627b70cec96a86
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-02-19 16:06:31 +0100

    Add package text field in new screen dialog.

--- src/main/kotlin/newscreen/NewScreenJPanel.kt
+++ src/main/kotlin/newscreen/NewScreenJPanel.kt
@@ -1,25 +1,53 @@
 package newscreen
 
 import java.awt.Dimension
-import javax.swing.JComponent
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
 
+
 class NewScreenJPanel : JPanel() {
 
     val nameTextField = JTextField()
+    val packageTextField = JTextField()
 
     init {
-        layout = null
-        JLabel("Name: ").addAt(20, 50, 80, 16)
-        nameTextField.addAt(100, 45, 180, 26)
+        layout = GridBagLayout()
+        val constraints = GridBagConstraints()
+        constraints.apply {
+            fill = GridBagConstraints.NONE
+            gridx = 0
+            gridy = 0
+            weightx = 0.2
+        }
+        add(JLabel("Package: "), constraints)
+
+        constraints.apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 1
+            gridy = 0
+            weightx = 0.8
+        }
+        add(packageTextField, constraints)
+
+        constraints.apply {
+            fill = GridBagConstraints.NONE
+            gridx = 0
+            gridy = 1
+            weightx = 0.2
+        }
+        add(JLabel("Name: "), constraints)
+
+        constraints.apply {
+            fill = GridBagConstraints.HORIZONTAL
+            gridx = 1
+            gridy = 1
+            weightx = 0.8
+        }
+        add(nameTextField, constraints)
     }
 
     override fun getPreferredSize() = Dimension(300, 110)
-
-    private fun JComponent.addAt(x: Int, y: Int, width: Int, height: Int) {
-        setBounds(x, y, width, height)
-        this@NewScreenJPanel.add(this)
-    }
 }
