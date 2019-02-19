caefe342f2376c316b2c943e08987d9f0edb28cb
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-02-19 09:58:18 +0100

    Create files for saved settings.

--- src/main/kotlin/newscreen/FileCreator.kt
+++ src/main/kotlin/newscreen/FileCreator.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
+import settings.ScreenGeneratorComponent
 
 interface FileCreator {
 
@@ -15,9 +16,12 @@ class FileCreatorImpl(private val project: Project) : FileCreator {
 
     override fun createScreenFiles(screenName: String) {
         ApplicationManager.getApplication().runWriteAction {
+            val screenElements = ScreenGeneratorComponent.getInstance(project).settings.screenElements
             val directory = PsiManager.getInstance(project).findDirectory(project.baseDir)
-            val file = PsiFileFactory.getInstance(project).createFileFromText("$screenName.kt", JavaLanguage.INSTANCE, "Text")
-            directory!!.add(file)
+            screenElements.forEach {
+                val file = PsiFileFactory.getInstance(project).createFileFromText("$screenName${it.name}.kt", JavaLanguage.INSTANCE, "Text")
+                directory!!.add(file)
+            }
         }
     }
 }
