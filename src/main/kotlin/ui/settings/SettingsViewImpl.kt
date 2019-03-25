1d82913dce7cad5337f20d6efc9dba66e8c0e78e
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-03-25 12:09:46 +0100

    Lazy init SettingsPanel in SettingsViewImpl.

--- src/main/kotlin/ui/settings/SettingsViewImpl.kt
+++ src/main/kotlin/ui/settings/SettingsViewImpl.kt
@@ -2,6 +2,7 @@ package ui.settings
 
 import com.intellij.openapi.options.Configurable
 import com.intellij.openapi.project.Project
+import com.intellij.ui.LanguageTextField
 import data.repository.SettingsRepositoryImpl
 import model.FileType
 import model.ScreenElement
@@ -11,9 +12,9 @@ import java.awt.event.ActionListener
 import javax.swing.JComponent
 import javax.swing.event.DocumentListener
 
-class SettingsViewImpl(project: Project) : Configurable, SettingsView {
+class SettingsViewImpl(private val project: Project) : Configurable, SettingsView {
 
-    private val panel = SettingsPanel(project)
+    private var panel: SettingsPanel? = null
     private val presenter = SettingsPresenter(this, SettingsRepositoryImpl(project))
     private var nameDocumentListener: DocumentListener? = null
     private var templateDocumentListener: com.intellij.openapi.editor.event.DocumentListener? = null
@@ -21,10 +22,14 @@ class SettingsViewImpl(project: Project) : Configurable, SettingsView {
     private var fragmentDocumentListener: DocumentListener? = null
     private var fileNameDocumentListener: DocumentListener? = null
 
-    private val fileTypeActionListener: ActionListener = ActionListener { presenter.onFileTypeSelect(FileType.values()[panel.fileTypeComboBox.selectedIndex]) }
+    private val fileTypeActionListener: ActionListener = ActionListener {
+        onPanel {
+            presenter.onFileTypeSelect(FileType.values()[fileTypeComboBox.selectedIndex])
+        }
+    }
 
-    private var currentTemplateTextField = panel.codePanel.kotlinTemplateTextField
-    private var currentSampleTextField = panel.codePanel.kotlinSampleTextField
+    private var currentTemplateTextField: LanguageTextField? = null
+    private var currentSampleTextField: LanguageTextField? = null
 
     override fun isModified() = presenter.isModified
 
@@ -34,121 +39,130 @@ class SettingsViewImpl(project: Project) : Configurable, SettingsView {
 
     override fun reset() = presenter.onResetSettings()
 
-    override fun createComponent(): JComponent {
+    override fun createComponent(): JComponent? {
+        panel = SettingsPanel(project)
         presenter.onLoadView()
-        panel.create(presenter::onHelpClick)
+        onPanel {
+            create(presenter::onHelpClick)
+            currentTemplateTextField = codePanel.kotlinTemplateTextField
+            currentSampleTextField = codePanel.kotlinSampleTextField
+        }
         presenter.onViewCreated()
         return panel
     }
 
-    override fun setUpListeners() = panel.run {
+    override fun setUpListeners() = onPanel {
         toolbarDecorator.apply {
             setAddAction { presenter.onAddClick() }
-            setRemoveAction { presenter.onDeleteClick(panel.screenElementsList.selectedIndex) }
-            setMoveDownAction { presenter.onMoveDownClick(panel.screenElementsList.selectedIndex) }
-            setMoveUpAction { presenter.onMoveUpClick(panel.screenElementsList.selectedIndex) }
+            setRemoveAction { presenter.onDeleteClick(screenElementsList.selectedIndex) }
+            setMoveDownAction { presenter.onMoveDownClick(screenElementsList.selectedIndex) }
+            setMoveUpAction { presenter.onMoveUpClick(screenElementsList.selectedIndex) }
         }
         screenElementsList.addListSelectionListener {
             if (!it.valueIsAdjusting) presenter.onScreenElementSelect(screenElementsList.selectedIndex)
         }
     }
 
-    override fun addScreenElement(screenElement: ScreenElement) {
-        panel.screenElementsListModel.add(screenElement)
+    override fun addScreenElement(screenElement: ScreenElement) = onPanel {
+        screenElementsListModel.add(screenElement)
     }
 
-    override fun selectScreenElement(index: Int) {
-        panel.screenElementsList.selectedIndex = index
+    override fun selectScreenElement(index: Int) = onPanel {
+        screenElementsList.selectedIndex = index
     }
 
-    override fun showName(name: String) {
-        panel.nameTextField.text = name
+    override fun showName(name: String) = onPanel {
+        nameTextField.text = name
     }
 
-    override fun addTextChangeListeners() {
-        nameDocumentListener = panel.nameTextField.addTextChangeListener(presenter::onNameChange)
-        templateDocumentListener = currentTemplateTextField.addTextChangeListener(presenter::onTemplateChange)
-        fileNameDocumentListener = panel.fileNameTextField.addTextChangeListener(presenter::onFileNameChange)
-        panel.fileTypeComboBox.addActionListener(fileTypeActionListener)
+    override fun addTextChangeListeners() = onPanel {
+        nameDocumentListener = nameTextField.addTextChangeListener(presenter::onNameChange)
+        templateDocumentListener = currentTemplateTextField?.addTextChangeListener(presenter::onTemplateChange)
+        fileNameDocumentListener = fileNameTextField.addTextChangeListener(presenter::onFileNameChange)
+        fileTypeComboBox.addActionListener(fileTypeActionListener)
     }
 
-    override fun removeTextChangeListeners() {
-        nameDocumentListener?.let { panel.nameTextField.document.removeDocumentListener(it) }
-        templateDocumentListener?.let { currentTemplateTextField.document.removeDocumentListener(it) }
-        fileNameDocumentListener?.let { panel.fileNameTextField.document.removeDocumentListener(it) }
+    override fun removeTextChangeListeners() = onPanel {
+        nameDocumentListener?.let { nameTextField.document.removeDocumentListener(it) }
+        templateDocumentListener?.let { currentTemplateTextField?.document?.removeDocumentListener(it) }
+        fileNameDocumentListener?.let { fileNameTextField.document.removeDocumentListener(it) }
         nameDocumentListener = null
         templateDocumentListener = null
         fileNameDocumentListener = null
-        panel.fileTypeComboBox.removeActionListener(fileTypeActionListener)
+        fileTypeComboBox.removeActionListener(fileTypeActionListener)
     }
 
-    override fun updateScreenElement(index: Int, screenElement: ScreenElement) =
-            panel.screenElementsListModel.setElementAt(screenElement, index)
+    override fun updateScreenElement(index: Int, screenElement: ScreenElement) = onPanel {
+        screenElementsListModel.setElementAt(screenElement, index)
+    }
 
-    override fun removeScreenElement(index: Int) = panel.screenElementsListModel.remove(index)
+    override fun removeScreenElement(index: Int) = onPanel { screenElementsListModel.remove(index) }
 
-    override fun showScreenElements(screenElements: List<ScreenElement>) =
-            screenElements.forEach { panel.screenElementsListModel.add(it) }
+    override fun showScreenElements(screenElements: List<ScreenElement>) = onPanel {
+        screenElements.forEach { screenElementsListModel.add(it) }
+    }
 
-    override fun clearScreenElements() = panel.screenElementsListModel.removeAll()
+    override fun clearScreenElements() = onPanel { screenElementsListModel.removeAll() }
 
     override fun showSampleCode(text: String) {
-        currentSampleTextField.text = text
+        currentSampleTextField?.text = text
     }
 
     override fun showTemplate(template: String) {
-        currentTemplateTextField.text = template
+        currentTemplateTextField?.text = template
     }
 
-    override fun showActivityBaseClass(text: String) {
-        panel.activityTextField.text = text
+    override fun showActivityBaseClass(text: String) = onPanel {
+        activityTextField.text = text
     }
 
-    override fun showFragmentBaseClass(text: String) {
-        panel.fragmentTextField.text = text
+    override fun showFragmentBaseClass(text: String) = onPanel {
+        fragmentTextField.text = text
     }
 
-    override fun addBaseClassTextChangeListeners() {
-        activityDocumentListener = panel.activityTextField.addTextChangeListener(presenter::onActivityBaseClassChange)
-        fragmentDocumentListener = panel.fragmentTextField.addTextChangeListener(presenter::onFragmentBaseClassChange)
+    override fun addBaseClassTextChangeListeners() = onPanel {
+        activityDocumentListener = activityTextField.addTextChangeListener(presenter::onActivityBaseClassChange)
+        fragmentDocumentListener = fragmentTextField.addTextChangeListener(presenter::onFragmentBaseClassChange)
     }
 
-    override fun removeBaseClassTextChangeListeners() {
-        activityDocumentListener?.let { panel.activityTextField.document.removeDocumentListener(it) }
+    override fun removeBaseClassTextChangeListeners() = onPanel {
+        activityDocumentListener?.let { activityTextField.document.removeDocumentListener(it) }
         activityDocumentListener = null
-        fragmentDocumentListener?.let { panel.fragmentTextField.document.removeDocumentListener(it) }
+        fragmentDocumentListener?.let { fragmentTextField.document.removeDocumentListener(it) }
         fragmentDocumentListener = null
     }
 
-    override fun showFileType(fileType: FileType) {
-        panel.fileTypeComboBox.selectedIndex = fileType.ordinal
+    override fun showFileType(fileType: FileType) = onPanel {
+        fileTypeComboBox.selectedIndex = fileType.ordinal
     }
 
-    override fun showCodeTextFields(fileType: FileType) = panel.codePanel.show(fileType)
+    override fun showCodeTextFields(fileType: FileType) = onPanel { codePanel.show(fileType) }
 
-    override fun swapToKotlinTemplateListener(addListener: Boolean) {
-        templateDocumentListener?.let { currentTemplateTextField.document.removeDocumentListener(it) }
-        currentTemplateTextField = panel.codePanel.kotlinTemplateTextField
-        currentSampleTextField = panel.codePanel.kotlinSampleTextField
-        if (addListener) templateDocumentListener = currentTemplateTextField.addTextChangeListener(presenter::onTemplateChange)
+    override fun swapToKotlinTemplateListener(addListener: Boolean) = onPanel {
+        templateDocumentListener?.let { currentTemplateTextField?.document?.removeDocumentListener(it) }
+        currentTemplateTextField = codePanel.kotlinTemplateTextField
+        currentSampleTextField = codePanel.kotlinSampleTextField
+        if (addListener) templateDocumentListener = currentTemplateTextField?.addTextChangeListener(presenter::onTemplateChange)
     }
 
-    override fun swapToXmlTemplateListener(addListener: Boolean) {
-        templateDocumentListener?.let { currentTemplateTextField.document.removeDocumentListener(it) }
-        currentTemplateTextField = panel.codePanel.xmlTemplateTextField
-        currentSampleTextField = panel.codePanel.xmlSampleTextField
-        if (addListener) templateDocumentListener = currentTemplateTextField.addTextChangeListener(presenter::onTemplateChange)
+    override fun swapToXmlTemplateListener(addListener: Boolean) = onPanel {
+        templateDocumentListener?.let { currentTemplateTextField?.document?.removeDocumentListener(it) }
+        currentTemplateTextField = codePanel.xmlTemplateTextField
+        currentSampleTextField = codePanel.xmlSampleTextField
+        if (addListener) templateDocumentListener = currentTemplateTextField?.addTextChangeListener(presenter::onTemplateChange)
     }
 
-    override fun showFileNameTemplate(text: String) {
-        panel.fileNameTextField.text = text
+    override fun showFileNameTemplate(text: String) = onPanel {
+        fileNameTextField.text = text
     }
 
-    override fun showFileNameSample(text: String) {
-        panel.fileNameSampleLabel.text = text
+    override fun showFileNameSample(text: String) = onPanel {
+        fileNameSampleLabel.text = text
     }
 
     override fun showHelp() = HelpDialog().show()
 
-    override fun setScreenElementDetailsEnabled(isEnabled: Boolean) = panel.setScreenElementDetailsEnabled(isEnabled)
+    override fun setScreenElementDetailsEnabled(isEnabled: Boolean) = onPanel { setScreenElementDetailsEnabled(isEnabled) }
+
+    private inline fun onPanel(function: SettingsPanel.() -> Unit) = panel?.run { function() } ?: Unit
 }
