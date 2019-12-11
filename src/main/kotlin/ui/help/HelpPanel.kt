e15b80e11387f009a3c6fd7ee580c3206c51e2ec
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-12-11 21:10:06 +0100

    Refactor HelpPanel to swing components.

--- src/main/kotlin/ui/help/HelpPanel.kt
+++ src/main/kotlin/ui/help/HelpPanel.kt
@@ -1,8 +1,10 @@
 package ui.help
 
-import com.intellij.ui.layout.panel
 import model.Variable
 import java.awt.BorderLayout
+import java.awt.FlowLayout
+import javax.swing.BoxLayout
+import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
 
@@ -10,15 +12,17 @@ class HelpPanel : JPanel() {
 
     init {
         layout = BorderLayout()
-        val panel = panel {
-            row { label("Available variables in templates:") }
+        val panel = JPanel().apply {
+            layout = BoxLayout(this, BoxLayout.Y_AXIS)
             Variable.values().forEach {
-                row {
-                    JTextField(it.value).apply { isEditable = false }()
-                    label(" - ${it.description}")
-                }
+                add(JPanel().apply {
+                    layout = FlowLayout(FlowLayout.LEADING)
+                    add(JTextField(it.value).apply { isEditable = false })
+                    add(JLabel(" - ${it.description}"))
+                })
             }
         }
+        add(JLabel("Available variables in templates:"), BorderLayout.NORTH)
         add(panel, BorderLayout.CENTER)
     }
 }
