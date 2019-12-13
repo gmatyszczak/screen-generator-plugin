e3dbc061be33d9c8fea54a5fec96770a7e53abf8
Grzegorz Matyszczak <grzesiek.matyszczak@gmail.com>
2019-12-13 23:02:29 +0100

    Refactor SettingsPanel to swing components.

--- src/main/kotlin/ui/settings/SettingsPanel.kt
+++ src/main/kotlin/ui/settings/SettingsPanel.kt
@@ -3,14 +3,13 @@ package ui.settings
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.CollectionListModel
+import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.JBSplitter
 import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.components.JBList
-import com.intellij.ui.layout.LCFlags
-import com.intellij.ui.layout.panel
 import model.FileType
 import model.ScreenElement
-import java.awt.BorderLayout
+import java.awt.*
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
@@ -50,35 +49,62 @@ class SettingsPanel(project: Project) : JPanel() {
         screenElementDetailsPanel = createScreenElementDetailsPanel()
 
         val rightPanel = createSplitterRightPanel(androidComponentsPanel, screenElementDetailsPanel)
-        val leftPanel = createSplitterLeftPanel(screenElementsPanel)
 
-        addSplitter(leftPanel, rightPanel)
+        addSplitter(screenElementsPanel, rightPanel)
         addCodePanel(onHelpClick)
     }
 
-    private fun createScreenElementsPanel() =
-            panel(LCFlags.fillX, title = "Screen Elements") {
-                row { toolbarDecorator.createPanel()(growX, growY, pushY) }
-            }
+    private fun createScreenElementsPanel() = JPanel().apply {
+        border = IdeBorderFactory.createTitledBorder("Screen Elements", false)
+        layout = GridLayout(1, 1)
+        add(toolbarDecorator.createPanel())
+    }
 
-    private fun createAndroidComponentsPanel() =
-            panel(LCFlags.fillX, title = "Android Components") {
-                row("Activity Base Class:") { activityTextField() }
-                row("Fragment Base Class:") { fragmentTextField() }
-            }
+    private fun createAndroidComponentsPanel() = JPanel().apply {
+        border = IdeBorderFactory.createTitledBorder("Android Components", false)
+        layout = GridBagLayout()
+        add(JLabel("Activity Base Class:"), constraintsLeft(0, 0))
+        add(activityTextField, constraintsRight(1, 0))
+        add(JLabel("Fragment Base Class:"), constraintsLeft(0, 1))
+        add(fragmentTextField, constraintsRight(1, 1))
+    }
 
-    private fun createScreenElementDetailsPanel() =
-            panel(LCFlags.fillX, title = "Screen Element Details") {
-                row(screenElementNameLabel) {
-                    nameTextField() }
-                row(fileNameLabel) {
-                    fileNameTextField()
-                    right { fileNameSampleLabel() }
-                }
-                row(fileTypeLabel) {
-                    fileTypeComboBox()
-                }
-            }
+    private fun createScreenElementDetailsPanel() = JPanel().apply {
+        border = IdeBorderFactory.createTitledBorder("Screen Element Details", false)
+        layout = GridBagLayout()
+        add(screenElementNameLabel, constraintsLeft(0, 0))
+        add(nameTextField, constraintsRight(1, 0))
+        add(fileNameLabel, constraintsLeft(0, 1))
+        add(fileNameTextField, constraintsRight(1, 1))
+        add(fileTypeLabel, constraintsLeft(0, 2))
+        add(fileTypeComboBox, constraintsRight(1, 2).apply {
+            gridwidth = 1
+            fill = GridBagConstraints.NONE
+            anchor = GridBagConstraints.LINE_START
+        })
+        add(fileNameSampleLabel, constraintsRight(2, 2).apply {
+            gridwidth = 1
+            weightx /= 2
+            anchor = GridBagConstraints.LINE_END
+            fill = GridBagConstraints.NONE
+        })
+    }
+
+    private fun constraintsLeft(x: Int, y: Int) = GridBagConstraints().apply {
+        weightx = 0.15
+        gridx = x
+        gridy = y
+        insets = Insets(0, 8, 0, 0)
+        fill = GridBagConstraints.HORIZONTAL
+    }
+
+    private fun constraintsRight(x: Int, y: Int) = GridBagConstraints().apply {
+        weightx = 0.85
+        gridx = x
+        gridy = y
+        gridwidth = 2
+        fill = GridBagConstraints.HORIZONTAL
+    }
 
     private fun addSplitter(leftPanel: JPanel, rightPanel: JPanel) {
         add(JBSplitter(0.2f).apply {
@@ -88,14 +114,9 @@ class SettingsPanel(project: Project) : JPanel() {
     }
 
     private fun createSplitterRightPanel(androidComponentsPanel: JPanel, screenElementDetailsPanel: JPanel) =
-            panel(LCFlags.fillX) {
-                row { androidComponentsPanel(growX) }
-                row { screenElementDetailsPanel(growX) }
-            }
-
-    private fun createSplitterLeftPanel(screenElementsPanel: JPanel) =
-            panel(LCFlags.fillX) {
-                row { screenElementsPanel(growX, growY, pushY) }
+            JPanel(GridLayout(0, 1)).apply {
+                add(androidComponentsPanel)
+                add(screenElementDetailsPanel)
             }
 
     private fun addCodePanel(onHelpClick: () -> Unit) {
